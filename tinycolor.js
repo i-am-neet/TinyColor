ab271542bcad95ed41ff0f1699a505c947c83490
NEET <zxc455233@gmail.com>
2021-08-25 20:03:05 +0800

    Add decimal point mode for RGB

--- tinycolor.js
+++ tinycolor.js
@@ -2,6 +2,54 @@
 // https://github.com/bgrins/TinyColor
 // Brian Grinstead, MIT License
 
+(function() {
+  /**
+   * Decimal adjustment of a number.
+   *
+   * @param {String}  type  The type of adjustment.
+   * @param {Number}  value The number.
+   * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+   * @returns {Number} The adjusted value.
+   */
+  function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+  }
+
+  // Decimal round
+  if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+      return decimalAdjust('round', value, exp);
+    };
+  }
+  // Decimal floor
+  if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+      return decimalAdjust('floor', value, exp);
+    };
+  }
+  // Decimal ceil
+  if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+      return decimalAdjust('ceil', value, exp);
+    };
+  }
+})();
+
 (function(Math) {
 
 var trimLeft = /^\s+/,
@@ -137,6 +185,11 @@ tinycolor.prototype = {
         return (this._a == 1) ?
           "rgb("  + mathRound(bound01(this._r, 255) * 100) + "%, " + mathRound(bound01(this._g, 255) * 100) + "%, " + mathRound(bound01(this._b, 255) * 100) + "%)" :
           "rgba(" + mathRound(bound01(this._r, 255) * 100) + "%, " + mathRound(bound01(this._g, 255) * 100) + "%, " + mathRound(bound01(this._b, 255) * 100) + "%, " + this._roundA + ")";
+    },
+    toPointRgbString: function() {
+        return (this._a == 1) ?
+          "rgb("  + Math.round10(bound01(this._r, 255), -2) + ", " + Math.round10(bound01(this._g, 255), -2) + ", " + Math.round10(bound01(this._b, 255), -2) + ")" :
+          "rgba(" + Math.round10(bound01(this._r, 255), -2) + ", " + Math.round10(bound01(this._g, 255), -2) + ", " + Math.round10(bound01(this._b, 255), -2) + ", " + this._roundA + ")";
     },
     toName: function() {
         if (this._a === 0) {
